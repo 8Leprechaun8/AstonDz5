28ff99c1813ec1d7ad2759b0d5f4891dcff9d41e
8Leprechaun8 <dmvoyevodin@mail.ru>
2026-05-01 09:42:25 +0300

    Добавление каркаса проекта

---
+++ src/main/java/org/example/repository/impl/FileManagerStudent.java
@@ -0,0 +1,6 @@
+package org.example.repository.impl;
+
+import org.example.repository.StudentRepository;
+
+public class FileManagerStudent implements StudentRepository {
+}
---
+++ src/main/java/org/example/sort/impl/SortByGroupNumber.java
@@ -0,0 +1,14 @@
+package org.example.sort.impl;
+
+import org.example.entity.Student;
+import org.example.sort.Sort;
+
+import java.util.List;
+
+public class SortByGroupNumber implements Sort {
+
+    @Override
+    public List<Student> sort(List<Student> studentList) {
+        return List.of();
+    }
+}
