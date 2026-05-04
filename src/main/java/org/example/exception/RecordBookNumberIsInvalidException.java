6cb0de9a764fd3007b89cce686bbaf4a94c0338f
8Leprechaun8 <dmvoyevodin@mail.ru>
2026-05-04 13:33:36 +0300

    Добавление ввода данных вручную и рандомно + валидация введенных данных.

---
+++ src/main/java/org/example/exception/RecordBookNumberIsInvalidException.java
@@ -0,0 +1,8 @@
+package org.example.exception;
+
+public class RecordBookNumberIsInvalidException extends Exception {
+
+    public RecordBookNumberIsInvalidException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/example/validation/Validator.java
+++ src/main/java/org/example/validation/Validator.java
@@ -1,4 +1,16 @@
 package org.example.validation;
 
+import org.example.entity.Student;
+import org.example.exception.AverageGradeIsOutOfBoundsException;
+import org.example.exception.RecordBookNumberIsFoundException;
+import org.example.exception.RecordBookNumberIsInvalidException;
+
+import java.util.List;
+
 public interface Validator {
+
+    void validateRecordBookNumber(Student student, List<Student> studentList)
+            throws RecordBookNumberIsFoundException, RecordBookNumberIsInvalidException;
+
+    void validateAverageGrade(Student student) throws AverageGradeIsOutOfBoundsException;
 }
